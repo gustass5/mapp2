2b8c5f2e83dfecb1670c9391bf64c6602aa9fbd3
Gustas <gustass1@gmail.com>
2021-03-30 22:51:04 +0300

    feat: added drawer component

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,47 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Mapp'),
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            Container(
+              height: 64.0,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                ),
+                child: Text(
+                  'Mapp Drawer',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Notifications'),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text('Info'),
+            ),
+            ListTile(
+              leading: Icon(Icons.ramen_dining),
+              title: Text('Whatever'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
