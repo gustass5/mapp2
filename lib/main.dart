46c10169e816bdc8553a773c5a4301e4e00487b7
Gustas <gustass1@gmail.com>
2021-03-30 22:52:35 +0300

    feat: added bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -66,6 +66,46 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigation(),
+    );
+  }
+}
+
+class BottomNavigation extends StatefulWidget {
+  @override
+  _BottomNavigationState createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int _selectedIndex = 1;
+
+  void _onNavigationItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      onTap: _onNavigationItemTapped,
+      backgroundColor: Colors.grey[900],
+      selectedItemColor: Colors.pink,
+      items: [
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.home),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.notes),
+          label: "Notebook",
+        ),
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.list),
+          label: "List",
+        ),
+      ],
     );
   }
 }
